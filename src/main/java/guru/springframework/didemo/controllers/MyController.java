ab46bbbd3025fbb7838065c727813dad910ead65
SancraianR <radu.sancraian@paddypowerbetfair.com>
2018-05-25 17:06:14 +0300

    First Commit Dependency injection

---
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,15 @@
+package guru.springframework.didemo.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!");
+
+        return "foo";
+    }
+
+}
